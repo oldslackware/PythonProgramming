7d606fc543db4e810a2ccdecdd404fbfa15b470c
oldslackware <oldslackware@gmail.com>
2019-02-16 03:22:07 +0100

    Learning Python

--- 3-IntroVariables.py
+++ 3-IntroVariables.py
@@ -4,11 +4,11 @@ anni= 38
 giorni=112.5
 nome="Luciano"
 
-print nome
-print giorni
-print anni
+print (nome)
+print (giorni)
+print (anni)
 
 nome,giorni,anni="Francesca",75.3,35
-print nome
-print giorni
-print anni
+print (nome)
+print (giorni)
+print (anni)
--- 4-StringOperation.py
+++ 4-StringOperation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-saluto=" Hello World "
-print saluto
-print saluto[1]
-print saluto[1:6]
-print saluto[7: ]
-print saluto *3
-print saluto +" and Universe"
+saluto=("Hello World ")
+print(saluto)
+print(saluto[1])
+print(saluto[0:5])
+print(saluto[6: ])
+print(saluto *3)
+print(saluto +" and Universe")
 
