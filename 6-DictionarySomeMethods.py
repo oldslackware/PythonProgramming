f5f7e61f08af5cdbf4efc7479dc9ddccad063b32
oldslackware <oldslackware@gmail.com>
2019-02-17 06:21:52 +0100

    Learning Python

--- 6-DictionarySomeMethods.py
+++ 6-DictionarySomeMethods.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 travelBestEurope={"Italy":"Florence","England":"Liverpool","Swisse":"Geneve","Norway":"Drammen","Belgie":"Bruges"}
 print(travelBestEurope)
+print("A better way to print")
+print(str(travelBestEurope))
 "print(travelBestEurope[1]) ERROR:->You can't access like array"
 print("My favourite city in Norway is: "+travelBestEurope["Norway"])
 "Add an element"
@@ -48,6 +50,30 @@ print(travelCapitalVisited)
 print("Show me the Nation with Status Capital")
 for (i,j),(i2,j2) in zip(travelBestEurope.items(),travelCapitalVisited.items()):
      print(i,j2)
+print("Nested dictionaries")
+"The number is the key and all inside {} is the value"
+car={
+     1:{"brand":"Renault","model":"Megane","price":"25000"},
+     2:{"brand":"Fiat","model":"Cinquecento","price":"15000"}
+}
+print(car)
+print("We try to add this dictionary(Note the color)")
+car[3]={"brand":"Nissan","model":"Micra","price":"9000","color":"red"}
+print(car[3])
+print("The new dictionary after the add")
+print(car)
+print("WE Remove the color from key 3")
+del car[3]["color"]
+print(car)
+print("Remove the dictionary Nissan")
+del car[3]
+print(car)
+print("The iterator power")
+for c_id, c_info in car.items():
+     print("\nAuto ID:", c_id)
+     for key in c_info:
+          print(key + ":", c_info[key])
+
 
 
 
